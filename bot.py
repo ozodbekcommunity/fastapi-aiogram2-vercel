930c1eeb4dd8a40cb5e19b27c926f3e682b07657
ozodbekcommunity <ozodbekcommunity@gmail.com>
2025-11-15 16:06:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 # bot.py
-# Aiogram 2.25.1 konfiguratsiyasi
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
 
-from aiogram import Bot, Dispatcher, types
-
-# Bot tokenini shu yerga qo‘ying
 BOT_TOKEN = "8377704874:AAGSHaqX1b37EFx_lXGoBDvkcf_6QEsNHAM"
 
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
+router = Router()
+dp.include_router(router)
 
-@dp.message_handler(commands=["start"])
+@router.message(Command("start"))
 async def start_cmd(message: types.Message):
-    await message.answer("Salom! Serverless FastAPI + Aiogram 2.25.1 bot ishlayapti 🚀")
+    await message.answer("Salom! Aiogram 3 + FastAPI serverless bot ishlayapti 🚀")
