adfc27f08341113fee43f60beebe19dcfcf9852c
ozodbekcommunity <ozodbekcommunity@gmail.com>
2025-11-15 16:07:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 # main.py
-# FastAPI webhook serveri (Vercel serverless muhitiga mos)
-
 from fastapi import FastAPI, Request
-from bot import dp, bot
+from bot import bot, dp
 import asyncio
 
 app = FastAPI()
 
-# Webhook URL (Vercel deploy linkini qo‘yasiz)
-WEBHOOK_URL = "https://fastapi-aiogram2-webhook-uz.vercel.app/api/webhook"
+WEBHOOK_PATH = "/api/webhook"
+WEBHOOK_URL = "https://fastapi-aiogram3-webhook-uz.vercel.app" + WEBHOOK_PATH
 
 
 @app.on_event("startup")
 async def on_startup():
-    # Webhook o‘rnatish
     await bot.set_webhook(WEBHOOK_URL)
 
 
-@app.post("/api/webhook")
-async def telegram_webhook(request: Request):
+@app.post(WEBHOOK_PATH)
+async def webhook(request: Request):
     data = await request.json()
-
-    asyncio.create_task(dp.process_update(dp.updates_factory(data)))
-
+    asyncio.create_task(dp.feed_update(bot, data))
     return {"status": "ok"}
 
 
 @app.get("/")
 async def home():
-
     return {"status": "Bot is running"}
