fae9a53fb96952336d41b171659077e4f8cd42d2
ozodbekcommunity <ozodbekcommunity@gmail.com>
2025-11-15 16:15:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-# main.py
 from fastapi import FastAPI, Request
 from bot import bot, dp
+from aiogram import types
 import asyncio
 
 app = FastAPI()
 
 WEBHOOK_PATH = "/api/webhook"
-WEBHOOK_URL = "https://fastapi-aiogram3-webhook-uz.vercel.app" + WEBHOOK_PATH
-
-
-@app.on_event("startup")
-async def on_startup():
-    await bot.set_webhook(WEBHOOK_URL)
+WEBHOOK_URL = "https://fastapi-aiogram3-webhook-uz.vercel.app/api/webhook"
 
 
 @app.post(WEBHOOK_PATH)
-async def webhook(request: Request):
+async def telegram_webhook(request: Request):
     data = await request.json()
-    asyncio.create_task(dp.feed_update(bot, data))
-    return {"status": "ok"}
+
+    update = types.Update.model_validate(data)
+
+    asyncio.create_task(dp.feed_update(bot, update))
+
+    return {"ok": True}
 
 
 @app.get("/")
